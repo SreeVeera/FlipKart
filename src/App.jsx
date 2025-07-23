9b965ba8bac2697e9a142880a7a3b42f9b52d296
SreeVeera <veerabagu.e@icanio.com>
2025-07-23 18:07:15 +0530

    added the menu bar component to choose categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import Category  from 'C:/React/Flipkart/src/header/category.jsx';
 import Appbar from 'C:/React/Flipkart/src/header/appbar.jsx';
 import Product from 'C:/React/Flipkart/src/header/productcard.jsx';
 function App() {
   return (
     <>
     <Appbar/>
+    <Category/>
     <Product/>
+    
     </>
   );
 }

e13cad10c6bf34ad7ab92cfa8bba4d88decf1792
SreeVeera <veerabagu.e@icanio.com>
2025-07-28 11:01:29 +0530

    Styling has been made for the Home page

--- src/CardMulticarousel/ElectronicProduct.jsx
+++ src/CardMulticarousel/ElectronicProduct.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const ElectronicProduct = (props) => {
   return (
-  <div onClick={props.onClick} style={{ cursor: props.onClick ? 'pointer' : 'default' }}>
-    <img src={props.img} alt={props.alt} width="100px" height="100px" />
-    <h2>{props.name}</h2>
+  <div className="Electronicpro" onClick={props.onClick} style={{ cursor: props.onClick ? 'pointer' : 'default' }}>
+    <img className="productimage" src={props.img} alt={props.alt} width="100px" height="100px" />
+    <h2 className="proname">{props.name}</h2>
   </div>
   )
 }
--- src/header/Item.jsx
+++ src/header/Item.jsx
@@ -4,10 +4,11 @@ export default function Item(props)
     return (
         <Paper className="paper">
              <img className="carousel" src={props.item.image} alt="carousel" width="100%" height="100px" />
-
-            <Button className="CheckButton" variant="contained" >
+<div className="button">
+            <Button className="check" variant="contained" >
                 Check it out!
             </Button>
+            </div>
         </Paper>
     )
 }   
